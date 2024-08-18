8468ac26ad77a3c86c2abb4765a3f6c3a0909420
John Silver <kramz.mr@gmail.com>
2024-08-18 11:30:51 +0300

    reverse-int done

--- src/index.js
+++ src/index.js
@@ -1,3 +1,4 @@
 module.exports = function reverse (n) {
-  
-}
+  return n.toString().split("")[0] != "-" ? n.toString().split("").reverse().join("") : n.toString().split("").slice(1).reverse().join("")
+} 
+
